1599f738691a987137be3f9fc08f221890d56c81
wilsonify <tom.andrew.wilson@gmail.com>
2019-05-26 16:55:16 -0500

    refactor for test

--- src/euler_python_package/euler_python/easiest/p034.py
+++ src/euler_python_package/euler_python/easiest/p034.py
@@ -1,9 +1,36 @@
+"""
+
+
+145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.
+
+   Find the sum of all numbers which are equal to the sum of the factorial of
+   their digits.
+
+   Note: as 1! = 1 and 2! = 2 are not sums they are not included.
+"""
+
 import math
 
 
-def problem034():
+def factorial_digit_sum(input_n):
     """
 
+    :param input_n:
+    :return:
+    """
+    result = 0
+    while input_n >= 10000:
+        result += FACTORIAL_DIGITS_SUM_WITH_LEADING_ZEROS[input_n % 10000]
+        input_n //= 10000
+    return result + FACTORIAL_DIGITS_SUM_WITHOUT_LEADING_ZEROS[input_n]
+
+
+FACTORIAL_DIGITS_SUM_WITHOUT_LEADING_ZEROS = [sum(math.factorial(int(c)) for c in str(i)) for i in range(10000)]
+FACTORIAL_DIGITS_SUM_WITH_LEADING_ZEROS = [sum(math.factorial(int(c)) for c in str(i).zfill(4)) for i in range(10000)]
+
+
+def sum_factorial_digits(upper_bound=10000000):
+    """
 
     145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.
 
@@ -13,23 +40,22 @@ def problem034():
        Note: as 1! = 1 and 2! = 2 are not sums they are not included.
     """
 
-    # As stated in the problem, 1 = 1! and 2 = 2! are excluded.
-    # If a number has at least n >= 8 digits, then even if every digit is 9,
-    # n * 9! is still less than the number (which is at least 10^n).
-    ans = sum(i for i in range(3, 10000000) if i == factorial_digit_sum(i))
+    ans = sum(i for i in range(3, upper_bound) if i == factorial_digit_sum(i))
     return ans
 
 
-def factorial_digit_sum(n):
-    result = 0
-    while n >= 10000:
-        result += FACTORIAL_DIGITS_SUM_WITH_LEADING_ZEROS[n % 10000]
-        n //= 10000
-    return result + FACTORIAL_DIGITS_SUM_WITHOUT_LEADING_ZEROS[n]
+def problem034():
+    """
+
+    # As stated in the problem, 1 = 1! and 2 = 2! are excluded.
+    # If a number has at least n >= 8 digits, then even if every digit is 9,
+    # n * 9! is still less than the number (which is at least 10^n).
 
 
-FACTORIAL_DIGITS_SUM_WITHOUT_LEADING_ZEROS = [sum(math.factorial(int(c)) for c in str(i)) for i in range(10000)]
-FACTORIAL_DIGITS_SUM_WITH_LEADING_ZEROS = [sum(math.factorial(int(c)) for c in str(i).zfill(4)) for i in range(10000)]
+    :return:
+    """
+    return sum_factorial_digits(upper_bound=10000000)
+
 
 if __name__ == "__main__":
     print(problem034())
